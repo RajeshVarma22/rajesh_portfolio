283fc17f91f63c090359f954287773c2026fa5c2
RajeshVarma22 <rajeshvarma2203@gmail.com>
2024-03-27 00:21:28 +0545

    img transitoins

--- src/App.js
+++ src/App.js
@@ -13,39 +13,19 @@ import { motion as m } from "framer-motion";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [isLoading, setLoading] = useState(false);
-  const [isLoadingBars, setLoadingBars] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [isLoadingBars, setLoadingBars] = useState(true);
 
-  console.log("Top useEffect");
-
-  // useEffect(() => {
-  //   console.log("Entered useEffect before return");
-
-  //   return () => {
-  //     console.log("Entered useEffect");
-  //     setTimeout(() => {
-  //       setLoadingBars(true);
-  //     }, 5300);
-
-  //     setTimeout(() => {
-  //       setLoading(true);
-  //       console.log("setTimeout 6300 working");
-  //     }, 6300);
-  //     console.log("End useEffect");
-  //   };
-  // }, [isLoading]);
 
   useEffect(() => {
     console.log("Entered useEffect");
   
     const setLoadingTimeout = setTimeout(() => {
       setLoadingBars(true);
-      console.log("setTimeout 5300 working");
     }, 5300);
   
     const setLoadingBarsTimeout = setTimeout(() => {
       setLoading(true);
-      console.log("setTimeout 6300 working");
     }, 6300);
   
     return () => {
@@ -53,9 +33,8 @@ function App() {
       clearTimeout(setLoadingBarsTimeout);
       console.log("Cleanup useEffect");
     };
-  }, []); // No dependencies
+  }, []);
   
-  console.log("After useEffect");
 
   return (
     <div className="App" style={{ backgroundColor: "black" }}>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,75 +6,93 @@ import Button2 from "../Buttons/Button2";
 import "./home.scss";
 
 const Home = () => {
-  
+  // const scaleImg = {
+  //   hidden: {
+  //     transform: {
+  //       scale: 0,
+  //     },
+  //   },
+  //   show: {
+  //     transform: {
+  //       scale: 1,
+  //     },
+  //     transition: {
+  //       duration: "2s",
+  //     },
+  //   },
+  // };
+
   return (
-      <section id="Home" className="sections container m-4">
-        <div id="Name">
-          <m.div
-            id="greetings"
-            className="childElements"
-            whileInView={{ backgroundPosition: "left bottom" }}
+    <section id="Home" className="sections container m-4">
+      <div id="Name">
+        <m.div
+          id="greetings"
+          className="childElements"
+          whileInView={{ backgroundPosition: "left bottom" }}
+        >
+          <m.h1 variants={parentVarints} initial="hidden" whileInView="show">
+            Hi There!
+            <span className="wave" role="img" aria-labelledby="wave">
+              👋🏻
+            </span>
+          </m.h1>
+        </m.div>
+        <m.div
+          id="name"
+          className="childElements"
+          whileInView={{ backgroundPosition: "left bottom" }}
+        >
+          <m.h1 variants={parentVarints} initial="hidden" whileInView="show">
+            I am Rajesh Varma,
+          </m.h1>
+        </m.div>
+        <m.div
+          id="designation"
+          className="childElements"
+          whileInView={{ backgroundPosition: "left bottom" }}
+        >
+          <m.h3
+            variants={parentVarints}
+            initial="hidden"
+            whileInView="show"
+            style={{ color: "red" }}
           >
-            <m.h1 variants={parentVarints} initial="hidden" whileInView="show">
-              Hi There!
-              <span className="wave" role="img" aria-labelledby="wave">
-                👋🏻
-              </span>
-            </m.h1>
-          </m.div>
-          <m.div
-            id="name"
-            className="childElements"
-            whileInView={{ backgroundPosition: "left bottom" }}
+            <Typewriter />
+          </m.h3>
+        </m.div>
+        <div className="buttonsContainer">
+          <a
+            href="https://drive.google.com/file/d/1hUNlHDgRVXF4_RZDdQwGpHHsbFn128iW/view?usp=sharing"
+            target="_blank"
+            download="Rajesh_varma_P Resume"
           >
-            <m.h1 variants={parentVarints} initial="hidden" whileInView="show">
-              I am Rajesh Varma,
-            </m.h1>
-          </m.div>
-          <m.div
-            id="designation"
-            className="childElements"
-            whileInView={{ backgroundPosition: "left bottom" }}
-          >
-            <m.h3
-              variants={parentVarints}
-              initial="hidden"
-              whileInView="show"
-              style={{ color: "red" }}
-            >
-              <Typewriter />
-            </m.h3>
-          </m.div>
-          <div className="buttonsContainer">
-            <a
-              href="https://drive.google.com/file/d/1hUNlHDgRVXF4_RZDdQwGpHHsbFn128iW/view?usp=sharing"
-              target="_blank"
-              download="Rajesh_varma_P Resume"
-            >
-              <Button2
-                text={"Resume"}
-                path={
-                  "M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 232V334.1l31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31V232c0-13.3 10.7-24 24-24s24 10.7 24 24z"
-                }
-              />
-            </a>
             <Button2
-              text={"About Me"}
+              text={"Resume"}
               path={
-                "M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
+                "M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 232V334.1l31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31V232c0-13.3 10.7-24 24-24s24 10.7 24 24z"
               }
             />
-          </div>
-        </div>
-        <div id="myMainImageContainer">
-          <img
-            src={myProfile}
-            alt="My Profile"
-            id="myMainImage"
-            className="content myMainImage"
+          </a>
+          <Button2
+            text={"About Me"}
+            path={
+              "M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
+            }
           />
         </div>
-      </section>
+      </div>
+      <div id="myMainImageContainer">
+        <m.img
+          initial={{ scale: 0.5, y: "50%", x: "30%" }}
+          transition={{ duration: 1, ease: "easeIn" }}
+          animate={{ scale: 1, y: "0%", x: "0%" }}
+          src={myProfile}
+          alt="My Profile"
+          id="myMainImage"
+          className="content myMainImage"
+        />
+      </div>
+    </section>
   );
 };
 
